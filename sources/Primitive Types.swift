effcf216859049b24185c3f3a54daa4b91ec2612
Károly Lőrentey <karoly@lorentey.hu>
2017-06-20 16:58:32 +0200

    Add SipHasher.append(_:) for raw buffer slices.
    This makes the upgrade path to Swift 4 a lot smoother.

--- sources/Primitive Types.swift
+++ sources/Primitive Types.swift
@@ -7,6 +7,15 @@
 //
 
 extension SipHasher {
+    //MARK: Appending buffer slices
+    
+    /// Add the contents of `slice` to this hash.
+    ///
+    /// - Requires: `finalize()` hasn't been called on this instance yet.
+    public mutating func append(_ slice: RandomAccessSlice<UnsafeRawBufferPointer>) {
+        self.append(UnsafeRawBufferPointer(rebasing: slice))
+    }
+    
     //MARK: Appending Integers
 
     /// Add `value` to this hash.
